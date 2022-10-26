c029cddc632705eea020405f5fdfe9d91659170e
Besteven <stefanostei18@gmail.com>
2022-10-26 03:16:35 +0300

    Oops.

--- valid_pass.c
+++ valid_pass.c
@@ -1,13 +1,191 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int is_valid_password(char pwd[]);
 
-main() {
-   char pwd[100];
-   
+int main() {
+    char pwd[100];
+    int strength;
+    while(1){
+
+        printf("\n\nEnter password: ");
+        scanf("%s", pwd);
+
+        if(strength = is_valid_password(pwd))
+            break;
+
+        if(pwd[0] == 0x0A){
+            printf("fdasf");
+            return 1;
+        }
+
+    }
+
+    printf("Your password is valid and has a strength of %d", strength);
+
+    return 0;
+
 }
 
-int is_valid_password(char pwd[]) {
-    return 1;
+int is_valid_password(char pwd[]){
+
+    int strength = 0;
+
+    //a
+
+    if(strlen(pwd) < 8){
+
+        printf("Your password must contain be at least 8 characters");
+        return 0;
+
+    }
+
+    //b
+
+    int upperC = 0;
+    int lowerC = 0;
+    int digitC = 0;
+    int specialC = 0;
+
+    int upperCaseCounter = 0;
+    int lowerCaseCounter = 0;
+    int digitCounter = 0;
+    int specialCharCounter = 0;
+
+    for(int i = 0; i < strlen(pwd) - 1; i++){
+
+        printf("pwd[i] == %c, pwd[i+1] == %c, pwd[i+2] == %c, pwd[i+3] == %c \n", pwd[i], pwd[i+1], pwd[i+2], pwd[i+3]);
+        printf("\n%c == %c = %d \n", pwd[i], pwd[i+1], pwd[i] == pwd[i+1]);
+
+        if(pwd[i] == pwd[i+1] == pwd[i+2] == pwd[i+3]){
+
+            printf("\n%d\n", i);
+
+            printf("\n%c%c%c%c%c\n", pwd[i], pwd[i+1], pwd[i+2], pwd[i+3], pwd[i+4]);
+
+            printf("Your password is not allowed to contain more than 3 similar characters in a row");
+            return strength;
+        
+        }
+
+    }
+
+    for(int i = 0; i < strlen(pwd); i++){
+
+        //c
+
+        for(int j = 0; j < 26; j++){
+            
+            if(pwd[i] == j + 65){
+            
+                upperC == 1;
+                upperCaseCounter++;
+            
+            }
+
+            if(pwd[i] == j + 97){
+            
+                lowerC == 1;
+                lowerCaseCounter++;
+            
+            }
+
+        }
+
+        for(int j = 0; j < 10; j++){
+            
+            if(pwd[i] == j + 48){
+            
+                digitC == 1;
+                digitCounter++;
+
+            }
+
+        }
+
+        if(pwd[i] == '!' || pwd[i] == '@' || pwd[i] == '#' || pwd[i] == '$' || pwd[i] == '&' ||  pwd[i] == '_'){
+        
+            specialC == 1;
+            specialCharCounter++;
+        
+        }
+
+    }
+
+    if((upperCaseCounter + lowerCaseCounter + digitCounter + specialCharCounter) < strlen(pwd)){
+
+        printf("Your password contains invalid characters");
+        return strength;
+
+    }
+
+    if((upperC + lowerC + digitC + specialC) < 3){
+        
+        printf("Your password must contain characters from at least 3 categories");
+        return strength;
+    
+    }
+
+    //2.
+
+    if(strlen(pwd) == 8){
+        
+        strength = 10;
+        return strength;
+    
+    }
+
+    //3.
+
+    for(int i = 0; i < strlen(pwd); i++){
+
+        if(i >= 8 && (i + 1) % 4 == 0){
+         
+            strength += 2;
+
+        }
+
+        //4.
+
+        for(int j = 0; j < strlen(pwd); j++){
+            
+            if(i != j && pwd[i] == pwd[j]){
+
+                break;
+
+            }
+
+            else
+            
+                strength += 5;
+
+        }
+
+    }
+
+    //5.
+    
+    for(int i = 0; i < strlen(pwd); i++){
+
+        
+        if((pwd[i] == pwd[i+1] == pwd[i+2]) || (pwd[i] == pwd[i+1])){
+
+            strength -= 2;
+            i += 2;
+
+        }
+
+    }
+
+    //6.
+
+    if(upperC + lowerC + digitC + specialC == 4){
+
+        strength *= 2;
+
+    }
+        
+    return strength;
+
 }
